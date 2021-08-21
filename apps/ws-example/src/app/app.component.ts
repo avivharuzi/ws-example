93b39c6a1f9db20e4039068e9f1bf898296967ee
Aviv Haruzi <avivharuzi@gmail.com>
2021-08-21 18:03:03 +0300

    Implement chat module logic in app module

--- apps/ws-example/src/app/app.component.ts
+++ apps/ws-example/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { ChatFacade } from '@ws-example/chat';
 
 @Component({
   selector: 'ws-example-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  messages$ = this.chatFacade.messages$;
+  userDetails$ = this.chatFacade.userDetails$;
+  users$ = this.chatFacade.users$;
+
+  connect$ = this.chatFacade.connect();
+
+  constructor(private chatFacade: ChatFacade) {}
+
+  sendMessage(value: string): void {
+    this.chatFacade.sendMessage(value);
+  }
+}
--- apps/ws-example/src/app/app.module.ts
+++ apps/ws-example/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChatModule } from '@ws-example/chat';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ChatModule.forRoot({
+      wsUrl: environment.wsUrl,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
