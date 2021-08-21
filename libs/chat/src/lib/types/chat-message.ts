d1c32e8f5198d56d88efcdbf1cd3145609d57758
Aviv Haruzi <avivharuzi@gmail.com>
2021-08-21 16:06:01 +0300

    Add chat message types

---
+++ libs/chat/src/lib/types/chat-message.ts
@@ -0,0 +1,49 @@
+export interface ChatMessage {
+  type: ChatMessageType;
+  data: {
+    usersList?: ChatDataUsersList;
+    userJoin?: ChatDataUserJoin;
+    userLeave?: ChatDataUserLeave;
+    userDetails?: ChatDataUserDetails;
+    sendMessage?: ChatDataSendMessage;
+    acceptMessage?: ChatDataAcceptMessage;
+  };
+}
+
+export enum ChatMessageType {
+  UsersList = 'UsersList',
+  UserJoin = 'UserJoin',
+  UserLeave = 'UserJoin',
+  UserDetails = 'UsersDetails',
+  SendMessage = 'SendMessage',
+  AcceptMessage = 'AcceptMessage',
+}
+
+export interface ChatDataUsersList {
+  users: ChatUser[];
+}
+
+export interface ChatDataUserJoin {
+  user: ChatUser;
+}
+
+export interface ChatDataUserLeave {
+  user: ChatUser;
+}
+
+export interface ChatDataUserDetails {
+  user: ChatUser;
+}
+
+export interface ChatDataSendMessage {
+  content: string;
+}
+
+export interface ChatDataAcceptMessage {
+  user: ChatUser;
+  content: string;
+}
+
+export interface ChatUser {
+  username: string;
+}
