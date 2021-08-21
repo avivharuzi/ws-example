535cffac96752bc6b0df3245cb73cad5f3c4939a
Aviv Haruzi <avivharuzi@gmail.com>
2021-08-21 17:33:15 +0300

    Add ws url environment

--- apps/ws-example/src/environments/environment.prod.ts
+++ apps/ws-example/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  wsUrl: 'ws://localhost:8080',
 };
--- apps/ws-example/src/environments/environment.ts
+++ apps/ws-example/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  wsUrl: 'ws://localhost:8080',
 };
 
 /*
