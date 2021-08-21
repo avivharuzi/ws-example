16109ff13beb0368b10d74fa2af235f2114bd88c
Aviv Haruzi <avivharuzi@gmail.com>
2021-08-21 11:45:54 +0300

    Add ws-wrapper lib

---
+++ libs/ws-wrapper/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'ws-wrapper',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/ws-wrapper',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
